3ce599b54ca6ee7c273b2ae094a7b5ed57b91871
reyahfathima <reyahfathima53@gmail.com>
2026-04-18 15:34:13 +0530

    commit

---
+++ threejs-lab/main.js
@@ -0,0 +1,40 @@
+import * as THREE from "https://unpkg.com/three@0.150.1/build/three.module.js";
+
+const scene = new THREE.Scene();
+
+// Add background so you don't see white
+scene.background = new THREE.Color(0x000000);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Cube
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00
+});
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Animation
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
